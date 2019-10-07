6af0bab4e5bd7902af6e1cbc028f3457340aeb52
André Costa <andre.pires1040@gmail.com>
2019-10-07 18:20:27 +0100

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Projeto_Teste_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
